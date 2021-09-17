af11628ac66c3b9e0b3924050af69e20c970f094
Jimmy Choca <jchoca@gmail.com>
2021-09-17 11:25:32 -0700

    reveal the word when the game is lost

--- game/hangman/lib/hangman/game.ex
+++ game/hangman/lib/hangman/game.ex
@@ -12,7 +12,7 @@ defmodule Hangman.Game do
       letters: word |> String.codepoints
     }
   end
-  
+
   def new_game() do
     new_game(Dictionary.random_word)
   end
@@ -31,15 +31,15 @@ defmodule Hangman.Game do
     %{
       game_state: game.game_state,
       turns_left: game.turns_left,
-      letters:    game.letters |> reveal_guessed(game.used),
-      used:       game.used    |> MapSet.to_list |> Enum.sort
+      letters:    game.game_state == :lost && game.letters || game.letters |> reveal_guessed(game.used),
+      used:       game.used    |> MapSet.to_list |> Enum.sort,
     }
   end
 
 
   ############################################################
 
-  
+
   defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
@@ -75,10 +75,9 @@ defmodule Hangman.Game do
 
   defp reveal_letter(letter, _in_word = true),  do: letter
   defp reveal_letter(_letter, _not_in_word),    do: "_"
-  
+
   defp maybe_won(true), do: :won
   defp maybe_won(_),    do: :good_guess
 
   defp return_with_tally(game), do: { game, tally(game) }
-  
 end
